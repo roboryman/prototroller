45e2964dffd37933ac2b074be5a14414e4ccadd2
calebscode <calebomalley@ufl.edu>
2022-11-19 14:53:36 -0500

    Changed device name to Prototroller

--- master/src/usb_descriptors.c
+++ master/src/usb_descriptors.c
@@ -182,7 +182,7 @@ char const* string_desc_arr [] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
   "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Prototroller",              // 2: Product
   "123456",                      // 3: Serials, should use chip ID
 };
 
